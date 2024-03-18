8462371bdc6868e72e545656e6055c5461df85c8
DonaldKnut <openiyiibrahim@gmail.com>
2024-03-18 11:37:22 +0100

    fourth commit

---
+++ admissions.js
@@ -0,0 +1,11 @@
+// Function to display the video modal
+function showVideo() {
+  var modal = document.getElementById("videoModal");
+  modal.style.display = "block";
+}
+
+// Function to hide the video modal
+function hideVideo() {
+  var modal = document.getElementById("videoModal");
+  modal.style.display = "none";
+}
